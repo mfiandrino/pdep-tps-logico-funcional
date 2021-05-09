81f0a3200e1d60d16334e6e4b3266a76df59db07
Maximiliano Fiandrino <maxi.fiandrino@gmail.com>
2021-05-08 22:49:16 -0300

    Soluciones Ejercicio 1 y 2 de integrante 1

--- TP_Funcional.hs
+++ TP_Funcional.hs
@@ -1,28 +1,45 @@
 -- TP Funcional _ Fuente de los Deseos _ Martes noche 2021 _ K2054
---Ejercicio 1, punto B
 
+--Ejercicio 1
 data Persona = Persona {edad :: Int, suenios :: [String], nombre :: String, felicidonios :: Int, habilidades :: [String]} deriving Show
 
 --juan es un recurso para ejemplos
 juan = Persona {edad = 26, suenios = ["hacer algo nuevo"], nombre = "juan cruz", felicidonios = 50, habilidades = ["nada", "mas nada"] }
 
+--Punto A (Integrante 1: Maximiliano Fiandrino)
+type CoeficienteSatisfaccion = Int
+calcularCoefSatisfaccion :: Persona -> CoeficienteSatisfaccion
+calcularCoefSatisfaccion persona    | felicidonios persona > 100 = (felicidonios persona) * (edad persona)
+                                    | felicidonios persona <= 100 && felicidonios persona > 50 = (length.suenios) persona * (felicidonios persona)
+                                    | otherwise = div (felicidonios persona) 2
+
+--Punto B (Integrante 2: Rodrigo Mollon)
 gradoDeAmbicion :: Persona -> Int
 gradoDeAmbicion unaPersona | felicidonios unaPersona > 100 = (length.suenios $ unaPersona) * (felicidonios unaPersona)
                            | felicidonios unaPersona > 50 && felicidonios unaPersona <= 100 = (length.suenios $ unaPersona) * (edad unaPersona)
                            | otherwise = (2*).length.suenios $ unaPersona
 
-coeficienteDeSatisfaccion :: Persona -> Int --TEMPORAL only for testing
-coeficienteDeSatisfaccion unaPersona | felicidonios unaPersona > 100 = 30
-                           | felicidonios unaPersona > 50 && felicidonios unaPersona <= 100 = 1
-                           | otherwise = 20
 
---Ejercicio 2, punto B
---asumimos que existe una funcion coeficienteDeSatisfaccion creada por Integrante 1 en el punto 1.A
 
+--Ejercicio 2
+
+--Punto A (Integrante 1: Maximiliano Fiandrino)
+esNombreLargo :: Persona -> Bool
+esNombreLargo persona = ((>10).length.nombre) persona
+
+--Punto B (Integrante 2: Rodrigo Mollon)
 personaSuertuda :: Persona -> Bool
-personaSuertuda otraPersona = (((3*).coeficienteDeSatisfaccion $ otraPersona) `mod` 2) == 0
+personaSuertuda otraPersona = (((3*).calcularCoefSatisfaccion $ otraPersona) `mod` 2) == 0
 
---Ejercicio 3, punto B
 
+
+--Ejercicio 3
+
+--Punto A (Integrante 1: Maximiliano Fiandrino)
+
+
+--Punto B (Integrante 2: Rodrigo Mollon)
 cumplirSueño :: String -> [String] -> Persona -> Persona
 cumplirSueño "viajar" listaCiudades laPersona = Persona {edad = (1+).edad $ laPersona,suenios = suenios laPersona, nombre = nombre laPersona, felicidonios = ((100*).length $ listaCiudades) + (felicidonios laPersona), habilidades = habilidades laPersona }
+
+--Punto C (Integrante 3: Daniel Kesel)
