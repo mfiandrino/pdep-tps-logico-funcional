ddb6bec7837dc70946996c10c1abe156eaa70ccb
Daniel Kesel <Daniel.Kesel@ar.ey.com>
2021-05-09 18:54:13 -0300

    Agregue los puntos 1C 2C y 3C al programa principal como también el TXT del testing correspondiente

--- TP_Funcional.hs
+++ TP_Funcional.hs
@@ -5,6 +5,8 @@ data Persona = Persona {edad :: Int, suenios :: [String], nombre :: String, feli
 
 --Recursos para ejemplos
 juan = Persona {edad = 26, suenios = ["hacer algo nuevo"], nombre = "juan cruz", felicidonios = 50, habilidades = ["nada", "mas nada"]}
+pedro = Persona {edad = 26, suenios = ["hacer algo nuevo"], nombre = "san pedro", felicidonios = 105, habilidades = ["nada", "mas nada"] }
+judas = Persona {edad = 26, suenios = ["hacer algo nuevo"], nombre = "judas", felicidonios = 50, habilidades = ["nada", "mas nada"] }
 persona1 = Persona 25 ["Recibirse de ingeniero", "Ser programador"] "Maximiliano" 100 ["Pintura","Java"]
 persona2 = Persona 46 ["Comprar una bicicleta"] "Camila" 250 ["Decir palindromos"]
 persona3 = Persona 12 ["Recibirse de Arquitecto","Conocer una nueva persona"] "Juan Ignacio" 55 ["Construir maquetas","Photoshop"]
@@ -22,7 +24,8 @@ gradoDeAmbicion unaPersona | felicidonios unaPersona > 100 = (length.suenios $ u
                            | felicidonios unaPersona > 50 && felicidonios unaPersona <= 100 = (length.suenios $ unaPersona) * (edad unaPersona)
                            | otherwise = (2*).length.suenios $ unaPersona
 
-
+--Punto C (Integrante 3: Daniel Kesel) 
+    --Ejecución de pruebas : Mirar archivo de texto "Testing TP1 - Puntos 1C 2C 3C- Integrante 3.txt"
 
 --Ejercicio 2
 
@@ -34,7 +37,9 @@ esNombreLargo = ((>10).length.nombre)
 personaSuertuda :: Persona -> Bool
 personaSuertuda otraPersona = (((3*).calcularCoefSatisfaccion $ otraPersona) `mod` 2) == 0
 
-
+--Punto C (Integrante 3: Daniel Kesel)
+esNombreLindo :: Persona -> Bool
+esNombreLindo = ((== 'a').last.nombre)
 
 --Ejercicio 3
 
@@ -48,3 +53,5 @@ cumplirSueño :: String -> [String] -> Persona -> Persona
 cumplirSueño "viajar" listaCiudades laPersona = Persona {edad = (1+).edad $ laPersona,suenios = suenios laPersona, nombre = nombre laPersona, felicidonios = ((100*).length $ listaCiudades) + (felicidonios laPersona), habilidades = habilidades laPersona }
 
 --Punto C (Integrante 3: Daniel Kesel)
+unaPersonaSeEnamoraDeOtra :: Persona -> Persona -> Persona 
+unaPersonaSeEnamoraDeOtra enamorado deQuienSeEnamoro = Persona (edad enamorado) (suenios enamorado) (nombre enamorado) ((felicidonios enamorado) + (felicidonios deQuienSeEnamoro)) (habilidades enamorado)
