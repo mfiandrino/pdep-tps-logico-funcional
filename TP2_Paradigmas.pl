ebb5e47fcfef472467cd5b220326fb4886cd1e88
Daniel Kesel <Daniel.Kesel@ar.ey.com>
2021-08-08 17:01:32 -0300

    avance del tp

---
+++ TP2_Paradigmas.pl
@@ -0,0 +1,56 @@
+materia(algebra, 5).
+materia(am1, 5).
+materia(mateDiscreta, 3).
+materia(ayed, 5).
+matereia(arqCompu, 4).
+materia(syo, 3).
+esIntegradora(syo).
+materia(quimica, 3).
+materia(ingysoc, 4).
+materia(ads, 6).
+esIntegradora(ads).
+materia(sistDeRep, 3).
+materia(am2, 5).
+materia(sintaxis, 4).
+materia(fisica1, 5).
+mateira(paradigmas, 4).
+materia(ingles1, 4).
+esLibre(ingles).
+materia(proba, 6).
+materia(diseñoDeSistemas, 6).
+esIntegradora(diseñoDeSistemas).
+materia(sistemasOperativos, 8).
+materia(fisica2, 10).
+materia(economia, 6).
+materia(gestionDeDatos, 8).
+materia(ingles2, 4).
+esLibre(ingles2).
+materia(matematicaSuperior, 8).
+materia(legislacion, 4).
+materia(administracionDeRecursos, 6).
+esIntegradora(administracionDeRecursos).
+materia(ingenieriaDeSoftware, 6).
+materia(teoriaDeControl, 6).
+materia(comunicaciones, 8).
+materia(redesDeInformacion, 8).
+materia(investigacionOperativa, 10).
+materia(simulacion, 8).
+materia(proyectoFinal, 6).
+esIntegradora(proyectoFinal, 6).
+materia(inteligenciaArtificial, 6).
+materia(administracionGerencial, 6).
+materia(sistemasDeGestion, 8).
+
+
+materiasIniciales(algebra).
+materiasIniciales(aml).
+materiasIniciales(ingles1).
+materiasIniciales(syo).
+materiasIniciales(ayed).
+materiasIniciales(mateDiscreta).
+materiasIniciales(arqCompu).
+materiasIniciales(quimica).
+materiasIniciales(ingysoc).
+materiasIniciales(sistDeRep).
+materiasIniciales(fisica1).
+
