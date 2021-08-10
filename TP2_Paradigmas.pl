740b241c8a4874477c47fc16c23ed8c252aab566
Daniel Kesel <Daniel.Kesel@ar.ey.com>
2021-08-10 00:24:43 -0300

    avance del tp - integrante 3

--- TP2_Paradigmas.pl
+++ TP2_Paradigmas.pl
@@ -2,33 +2,27 @@ materia(algebra, 5).
 materia(am1, 5).
 materia(mateDiscreta, 3).
 materia(ayed, 5).
-matereia(arqCompu, 4).
+materia(arqCompu, 4).
 materia(syo, 3).
-esIntegradora(syo).
 materia(quimica, 3).
 materia(ingysoc, 4).
 materia(ads, 6).
-esIntegradora(ads).
 materia(sistDeRep, 3).
 materia(am2, 5).
 materia(sintaxis, 4).
 materia(fisica1, 5).
-mateira(paradigmas, 4).
+materia(paradigmas, 4).
 materia(ingles1, 4).
-esLibre(ingles).
 materia(proba, 6).
-materia(diseñoDeSistemas, 6).
-esIntegradora(diseñoDeSistemas).
+materia(disenioDeSistemas, 6).
 materia(sistemasOperativos, 8).
 materia(fisica2, 10).
 materia(economia, 6).
 materia(gestionDeDatos, 8).
 materia(ingles2, 4).
-esLibre(ingles2).
 materia(matematicaSuperior, 8).
 materia(legislacion, 4).
 materia(administracionDeRecursos, 6).
-esIntegradora(administracionDeRecursos).
 materia(ingenieriaDeSoftware, 6).
 materia(teoriaDeControl, 6).
 materia(comunicaciones, 8).
@@ -36,11 +30,24 @@ materia(redesDeInformacion, 8).
 materia(investigacionOperativa, 10).
 materia(simulacion, 8).
 materia(proyectoFinal, 6).
-esIntegradora(proyectoFinal, 6).
 materia(inteligenciaArtificial, 6).
 materia(administracionGerencial, 6).
 materia(sistemasDeGestion, 8).
+esIntegradora(ads).
+esIntegradora(syo).
+esIntegradora(disenioDeSistemas).
+esIntegradora(proyectoFinal).
+esIntegradora(administracionDeRecursos).
+esLibre(ingles).
+esLibre(ingles2).
 
+
+/* punto 2 */
+esCorrelativa(am2, am1).
+esCorrelativa(sintaxis, mateDiscreta).
+esCorrelativa(sintaxis, ayed).
+esCorrelativa(economia, ads).
+esCorrelativa(ads, syo).
 
 materiasIniciales(algebra).
 materiasIniciales(aml).
@@ -53,4 +60,13 @@ materiasIniciales(quimica).
 materiasIniciales(ingysoc).
 materiasIniciales(sistDeRep).
 materiasIniciales(fisica1).
+/* PUNTO 2 */
+% a.
+materiasIniciales(MateriasIniciales):-
+    findall(MateriaInicial, materiasIniciales(MateriaInicial), MateriasIniciales).
 
+
+% c.
+
+  /* materiasQueHabilita(Asignatura, MateriasQueHabilita):-materia(NombreMateria, _),
+    findall(MateriaQueHabilita, (necesariasParaCursar(MateriaQueHabilita, MateriasNecesarias), member(NombreMateria, MateriasNecesarias)), MateriasQueHabilita).*/
